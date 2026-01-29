cb1f58c08b6890eea7a73d596dbf08a0fdaf7b65
Musthak <musthakcool55@gmail.com>
2026-01-29 14:00:25 +0530

    social media app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["email", "username", "first_name", "last_name", "phone_number", "is_staff"]
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("country", "phone_number")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("country", "phone_number")}),
+    )
+
+    search_fields = ("username", "email", "phone_number")
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
