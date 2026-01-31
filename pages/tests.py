ad54d4b44da1afd16c1d9a412c43fd4929746fdb
Musthak <musthakcool55@gmail.com>
2026-01-31 10:20:38 +0530

    social media app

--- pages/tests.py
+++ pages/tests.py
@@ -13,3 +13,26 @@ class HomePageTests(SimpleTestCase):
         self.assertContains(response, 'Home')
         self.assertTemplateUsed(response, 'home.html')
 
+class PostPageView(SimpleTestCase):
+
+    def test_url_exists_at_correct_location_postpageview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_postpage_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Post')
+        self.assertTemplateUsed(response, "home.html")
+
+class PostCreateView(SimpleTestCase):
+
+    def test_url_exists_at_correct_location_postcreateview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_postcreate_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Post_Create')
+        self.assertTemplateUsed(response, "home.html")
